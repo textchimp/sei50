8f810ccff9f99783ef4a8cd884650e2a3d9b3c6e
Luke Hammer <textchimp@gmail.com>
2022-01-25 17:28:32 +1100

    wk5 day2: Rails with DB, Planets codealong

---
+++ week5/planets-db/app/models/planet.rb
@@ -0,0 +1,17 @@
+
+class Planet < ApplicationRecord
+
+  # This mode file tells ActiveRecord where to find your
+  # database table - it uses the pluralised lowercase
+  # version of this class name, i.e. 'planets'
+
+  # Later on, we will also specify and associations
+  # here, such as 'has_many :astronauts' etc
+
+  # Much later on, you can also define your own methods
+  # here to specify business logic for this model
+  #
+  # "Fat models, skinny controllers"
+
+
+end # class Planet
---
+++ week5/planets-db/config/routes.rb
@@ -0,0 +1,63 @@
+Rails.application.routes.draw do
+
+  # Create ##################################################
+
+  # 1. Blank form
+  # ROUTE: GET /planets/new
+  # CONTROLLER ACTION: planets#new
+  # TEMPLATE: new.html.erb
+  # AR QUERY: n/a
+  get '/planets/new' => 'planets#new', as: 'new_planet'
+
+  # 2. Form submits here, save to DB, redirect
+  # ROUTE: POST /planets
+  # CONTROLLER ACTION: planets#create
+  # TEMPLATE: n/a (redirect)
+  # AR QUERY: Planet.create(param fields here)
+  post '/planets' => 'planets#create'
+
+  # Read   ##################################################
+
+  # a. Index page, i.e. list every row of the 'planets' table
+  # ROUTE: GET /planets
+  # CONTROLLER ACTION: planets#index
+  # TEMPLATE: app/views/planets/index.html.erb
+  # AR QUERY: Planet.all
+  get '/planets' => 'planets#index'
+
+
+  # b. Show page, i.e. details about one row (planet) by ID
+  # ROUTE: GET /planets/:id
+  # CONTROLLER ACTION: planets#show
+  # TEMPLATE: show.html.erb
+  # AR QUERY: Planet.find params[:id]
+  get '/planets/:id' => 'planets#show', as: 'planet'
+                                        # gives us 'planet_path(ID_HERE)'
+
+  # Update ##################################################
+
+  # 1. Show pre-filled edit form
+  # ROUTE: GET /planets/:id/edit
+  # CONTROLLER ACTION: planets#edit
+  # TEMPLATE: edit.html.erb
+  # AR QUERY: Planet.find params[:id]
+  get '/planets/:id/edit' => 'planets#edit', as: 'edit_planet'
+
+  # 2. Form submits to here, do DB update, redirect
+  # ROUTE: PATCH /planets/:id  (PATCH is a new HTTP verb indicating an update)
+  # CONTROLLER ACTION: planets#update
+  # TEMPLATE: n/a  (redirect)
+  # AR QUERY: find + update
+  patch '/planets/:id' => 'planets#update'
+
+  # Even though the above form submit method is officially "post", the hidden form variable "_method" specifies "patch", which takes priority here. Soon this will be part of the HTTP spec, i.e. a new verb
+
+  # Delete ##################################################
+  # Just perform destroy by ID, and redirect
+  # ROUTE: GET /planets/:id/delete
+  # CONTROLLER ACTION: planets#destroy
+  # TEMPLATE: n/a  (redirect)
+  # AR QUERY: Planet.destroy params[:id]
+  get '/planets/:id/delete' => 'planets#destroy', as: 'destroy_planet'
+
+end

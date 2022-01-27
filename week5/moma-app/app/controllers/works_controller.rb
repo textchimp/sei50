da51c5bed9f41ebbd24e1ee6c09696fd2fa5c81b
Luke Hammer <textchimp@gmail.com>
2022-01-27 17:47:03 +1100

    codealong 17-47-03

--- week5/moma-app/app/controllers/works_controller.rb
+++ week5/moma-app/app/controllers/works_controller.rb
@@ -1,8 +1,12 @@
 class WorksController < ApplicationController
+
   def new
+    @work = Work.new
   end
 
   def create
+    # raise 'hell'
+    Work.create work_params  # we need to create 'work_params'
   end
 
   # READ
@@ -24,4 +28,10 @@ class WorksController < ApplicationController
   def destroy
   end
 
-end
+  private
+
+  def work_params
+    params.require(:work).permit( :title, :year, :medium, :style, :image, :artist_id )
+  end
+
+end  # class WorksController

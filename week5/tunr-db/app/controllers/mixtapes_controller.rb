654baef17e772def8f9dbd1471af75504c6086e7
Luke Hammer <textchimp@gmail.com>
2022-01-31 14:55:02 +1100

    codealong 14-55-02

--- week5/tunr-db/app/controllers/mixtapes_controller.rb
+++ week5/tunr-db/app/controllers/mixtapes_controller.rb
@@ -7,15 +7,35 @@ class MixtapesController < ApplicationController
   end
 
   def create
-    mixtape = Mixtape.create mixtape_params
+    # mixtape = Mixtape.create mixtape_params
+    # mixtape.user_id = @current_user.id  # OR: mixtape.user = @current_user
+    # mixtape.save  # NOT saved to the DB until we do this!
 
-    mixtape.user_id = @current_user.id  # OR: mixtape.user = @current_user
-    mixtape.save  # NOT saved to the DB until we do this!
+    @mixtape = Mixtape.new mixtape_params
+    @mixtape.user_id = @current_user.id
+    @mixtape.save
 
-    redirect_to mixtapes_path
-  end
+    # Did the above save work, or did it fail due to a validation error?
+    if @mixtape.persisted?
+      # all good
+      redirect_to mixtapes_path
+    else
+      # redirect_to new_mixtape_path # this empties the form!
+
+      # We can render the template of some OTHER action here
+      # In this case we are in :create, but we are asking to render
+      # the :new template
+      # This will provide the template with the already-filled out
+      # form fields, so the user doesn't have to type out all the
+      # correct ones again
+      render :new
+    end
+
+
+  end # create()
 
   def index
+    @mixtapes = Mixtape.all
   end
 
   def show
--- week5/tunr-db/app/models/mixtape.rb
+++ week5/tunr-db/app/models/mixtape.rb
@@ -4,4 +4,6 @@ class Mixtape < ApplicationRecord
 
   belongs_to :user, optional: true
 
+  validates :name, length: { minimum: 2 } 
+
 end

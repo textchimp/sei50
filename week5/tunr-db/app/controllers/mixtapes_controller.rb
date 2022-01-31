7f520fd755c848994e744ffc2c86b76fef947303
Luke Hammer <textchimp@gmail.com>
2022-01-31 13:05:16 +1100

    codealong 13-05-16

--- week5/tunr-db/app/controllers/mixtapes_controller.rb
+++ week5/tunr-db/app/controllers/mixtapes_controller.rb
@@ -3,9 +3,16 @@ class MixtapesController < ApplicationController
   before_action :check_if_logged_in, except: [:index, :show]
 
   def new
+    @mixtape = Mixtape.new
   end
 
   def create
+    mixtape = Mixtape.create mixtape_params
+
+    mixtape.user_id = @current_user.id  # OR: mixtape.user = @current_user
+    mixtape.save  # NOT saved to the DB until we do this!
+
+    redirect_to mixtapes_path
   end
 
   def index
@@ -21,6 +28,13 @@ class MixtapesController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  private
+
+  def mixtape_params
+    params.require(:mixtape).permit(:name, :image)
   end
 
 end

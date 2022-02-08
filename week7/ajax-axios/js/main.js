1b512a9bb72c3f7c421e842b84238e9665b19e0b
Luke Hammer <textchimp@gmail.com>
2022-02-08 18:20:54 +1100

    codealong 18-20-54

--- week7/ajax-axios/js/main.js
+++ week7/ajax-axios/js/main.js
@@ -24,6 +24,10 @@ const renderSearchResults = function( data ){
   console.log('renderSearchResults()', data);
   const $output = $('#output');
 
+  $output.show(); // in case some submits a search from a show page i.e. results are hidden
+  $('#details').hide();  // as above
+
+
   $output.html(`<p>${ data.total_results } matches found:</p>`);
 
   let movies = '<ul>';
@@ -31,8 +35,8 @@ const renderSearchResults = function( data ){
   data.results.forEach( function( movie ){
     // console.log( movie );
     movies += `
-      <li>
-        <img src="http://image.tmdb.org/t/p/w185${ movie.poster_path }" alt="${ movie.title }"> 
+      <li data-movie-id="${ movie.id }">
+        <img src="http://image.tmdb.org/t/p/w185${ movie.poster_path }" alt="${ movie.title }">
         <br>
         ${ movie.title }
       </li>
@@ -43,9 +47,55 @@ const renderSearchResults = function( data ){
 
   $output.append( movies );
 
+  // After the append above is done, add a click handler for
+  // all the new <li> tags
+  $('li').on('click', function(){
+    const movieID = $(this).data('movie-id');
+    console.log('Movie clicked', movieID );
+    fetchMovieByID( movieID );
+  });
+
 
 }; // renderSearchResults()
 
+
+const fetchMovieByID = function( id ){
+  console.log('fetchMovieByID(): ', id);
+
+  // Perform the AJAX request
+  const url = `https://api.themoviedb.org/3/movie/${ id }?api_key=24d863d54c86392e6e1df55b9a328755`;
+  axios.get( url )
+    .then( function( res ){
+      console.log('Movie info:', res.data );
+
+      // should have renderMovieDetails( res.data );
+      // instead after 6pm we cheat and use jQuery right here
+
+      // Hide the search results
+      $('#output').hide();
+      $('#details').show(); // because we might have hidden this div when click previous back button
+
+      $('#details').html(`
+        <h3>${ res.data.title }</h3>
+        <img src="http://image.tmdb.org/t/p/w185${ res.data.poster_path }" alt="${ res.data.title }">
+        <p>${ res.data.overview }</p>
+        <p>
+          <strong>Budget:</strong> US$${ res.data.budget }
+        </p>
+        <button id="backButton">Back to Results</button>
+      `);
+
+      $('#backButton').on('click', function(){
+        $('#details').hide();
+        $('#output').show();
+      }); // back button click handler
+
+    })
+    .catch( function( err ){
+      console.log('Error loading Movie:', err);
+    });
+
+}; // fetchMovieByID()
 
 
 $(function(){

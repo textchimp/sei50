41052f383e558bf5f3e2f465319f0f1d90d383bb
Luke Hammer <textchimp@gmail.com>
2022-02-10 17:51:49 +1100

    codealong 17-51-49

--- week7/vue-intro/js/main.js
+++ week7/vue-intro/js/main.js
@@ -1,5 +1,35 @@
 console.log('Loaded main.js', Vue);
 
+
+Vue.component('dog-info', {
+  props: [ 'name', 'roundness' ],
+  data: function(){
+    // Components must define data as a function that returns the
+    // data object, so that each instance gets a unique copy
+    return {
+      age: 5
+    };
+  },
+  methods: {
+    incrementAge: function(){
+      this.age++;
+    }
+  },
+  template: `
+    <div>
+      <h3 @click="incrementAge">
+        {{ name }}
+      </h3
+      <p>
+        Roundness: {{ roundness }}
+      </p>
+      <p>
+        Age: {{ age }}
+      </p>
+    </div>
+  `
+});
+
 const myApp = new Vue({
   // Where does this Vue app attach to the dom?
   el: '#app',
@@ -29,7 +59,10 @@ const myApp = new Vue({
       console.log('clicked!', ev);
       this.message = this.message.split('').reverse().join('');
       this.showMessage = !this.showMessage;
-    } // changeMessage()
+    }, // changeMessage()
+    created: function(){
+      console.log('Component loaded');
+    }
   },
 
 

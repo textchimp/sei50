7eb88fb62f3ef35959b2f322c1a0079d0fbcde41
Luke Hammer <textchimp@gmail.com>
2022-02-14 14:24:17 +1100

    codealong 14-24-17

--- week8/react-creepy-dentist/src/App.js
+++ week8/react-creepy-dentist/src/App.js
@@ -6,59 +6,44 @@ import TeethSales from './TeethSales'
 import Procedures from './Procedures'
 
 
-import { Route, HashRouter as Router } from 'react-router-dom';
+import { Route, HashRouter as Router, Link } from 'react-router-dom';
 
 class App extends React.Component {
-
-  // initialising state WITHOUT contstructor()
-  state = {
-    currentPage: 'default'
-  };
-
-
-  navigateTo = (destination) => {
-    console.log('navigateTo()', destination);
-    this.setState({ currentPage: destination })
-  }; // navigateTo()
 
 
   render(){
 
     return (
       <div className="App">
-        <header>
-          <h1>
-            Dentistry is my passion<br/>
-            It's my obsession.<br/>
-            I need this.
-          </h1>
-          <nav>
-
-            <a href="#" onClick={ () => this.navigateTo('home') }>Home</a> |&nbsp;
-            <a href="#" onClick={ () => this.navigateTo('sales') }>Teeth Sales</a> |&nbsp;
-            <a href="#" onClick={ () => this.navigateTo('procedures') }>Procedures</a>
-
-            <br/>
-            Current page: { this.state.currentPage }
-
-          </nav>
-          <hr/>
-        </header>
-
         <Router>
+          <header>
+            <h1>
+              Dentistry is my passion<br/>
+              It's my obsession.<br/>
+              I need this.
+            </h1>
+            <nav>
 
-          {
-            // This is like the Rails routes.rb file!
+              <Link to="/">Home</Link> |&nbsp;
+              <Link to="/sales">Teeth Sales</Link> |&nbsp;
+              <Link to="/procedures">Procedures</Link>
 
-            //  get '/sales'  => 'sales#teeth'
-          }
-          <Route exact path="/" component={ Home } />
-          <Route exact path="/sales" component={ TeethSales } />
-          <Route exact path="/procedures" component={ Procedures } />
+            </nav>
+            <hr/>
+          </header>
+
+
+            {
+              // This is like the Rails routes.rb file!
+
+              //  get '/sales'  => 'sales#teeth'
+            }
+            <Route exact path="/" component={ Home } />
+            <Route exact path="/sales" component={ TeethSales } />
+            <Route exact path="/procedures" component={ Procedures } />
+
 
         </Router>
-
-
       </div>
     );
 

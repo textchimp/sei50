49cb41df99f45eb8118e9a58348f1826b734d030
Luke Hammer <textchimp@gmail.com>
2022-02-14 15:15:30 +1100

    codealong 15-15-30

--- week8/react-creepy-dentist/src/ProcedureSearchResults.js
+++ week8/react-creepy-dentist/src/ProcedureSearchResults.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
+
+
+
 class ProcedureSearchResults extends React.Component {
+
+  // If you create a method with this name, it will automatically
+  // run when your component is first added to the DOM
+  // - a bit like the DOM ready function of jQuery: $(document).ready( fn );
+  // This is called a "lifecycle method"
+  componentDidMount(){
+    console.log('componentDidMount()');
+  } // componentDidMount()
+
+  componentWillUnmount(){
+    console.log('componentWillUnmount()');
+  } // componentDidMount()
 
 
   render(){
@@ -9,6 +24,12 @@ class ProcedureSearchResults extends React.Component {
     // const id = this.props.match.params.id;
     // const {params} = this.props.match;
     // const {query} = this.props.match.params;
+
+    // DON'T do an axios request here in the render()
+    // Why not? Because the request will happen every time a render() happens...
+    // and renders happen any time you update state in React! So it will happen
+    // too often! And worse, if you save your API response into state using setState(),
+    // you will have an infinite loop!
 
 
     return (

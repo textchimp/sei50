e1cd5b7ddd6737185824ecd02bfd389fc5f884fd
Luke Hammer <textchimp@gmail.com>
2022-02-28 17:26:45 +1100

    codealong 17-26-45

---
+++ week10/burning-airlines-immersive/ba-vue-frontend/src/components/FlightSearchResults.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Search Results</h2>
+    {{ $route.params.origin }}
+    {{ $route.params.destination }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FlightSearchResults'
+}
+</script>
+
+<style scoped>
+</style>

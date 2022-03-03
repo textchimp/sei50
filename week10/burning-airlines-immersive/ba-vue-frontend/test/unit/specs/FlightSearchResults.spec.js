734fd49d76966eb96799317d641a08761d8068da
Luke Hammer <textchimp@gmail.com>
2022-03-03 15:43:44 +1100

    codealong 15-43-43

--- week10/burning-airlines-immersive/ba-vue-frontend/test/unit/specs/FlightSearchResults.spec.js
+++ week10/burning-airlines-immersive/ba-vue-frontend/test/unit/specs/FlightSearchResults.spec.js
@@ -6,14 +6,56 @@ import FlightSearchResults from '@/components/FlightSearchResults';
 
 import axios from 'axios'; // so we can mock it
 
+const fakeFlights = [
+  {
+    id: 1,
+    flight_number: 'BA123',
+    departure_date: '2022-03-01T04:20:00.000Z',
+    origin: 'SYD',
+    destination: 'MEL',
+    airplane_id: 7,
+    created_at: '2022-02-28T01:44:21.490Z',
+    updated_at: '2022-02-28T01:44:21.490Z',
+    airplane: {
+      name: '747'
+    }
+  }, // First flight
+  {
+    id: 2,
+    flight_number: 'BA512',
+    departure_date: '2022-03-02T11:20:00.000Z',
+    origin: 'SYD',
+    destination: 'MEL',
+    airplane_id: 7,
+    created_at: '2022-02-28T01:44:21.504Z',
+    updated_at: '2022-02-28T01:44:21.504Z',
+    airplane: {
+      name: '737 MAX'
+    }
+  } // second flight
+];
+
 describe('<FlightSearchResults>', () => {
 
   let wrapper;
   beforeEach( () => {
+
     wrapper = mount(FlightSearchResults, {
       propsData: { origin: 'SYD', destination: 'MEL'  } // fake props (from router)
     });
+
+    // spy - fake version of a function that lets
+    //       you ask whether the function was called
+    //       or not by the application code, and
+    //       which arguments it was called with
+    // stub - a spy which ALSO lets you fake
+    //        the behaviour (i.e. the return value)
+    //        of the function you are faking
+
+    axios.get = sinon.stub().returns( Promise.resolve( {data: fakeFlights} ) );
+
   }); // beforeEach
+
 
 
   it('renders without errors', () => {
